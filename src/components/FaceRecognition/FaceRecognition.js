4b0161c3bd4cde7426ea1863d421488bbc233b40
edwinchen85 <edwinchen85@gmail.com>
2018-01-22 15:16:09 +0800

    Code Face Detection Box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
+  console.log(box);
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={imageUrl} alt="" width="500px" height="auto" />
+        <img id="inputImage" src={imageUrl} alt="" style={{maxWidth: "500px"}} width="100%" height="auto" />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        ></div>
       </div>
     </div>
   );
