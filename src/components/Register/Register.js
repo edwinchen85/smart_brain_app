d5ecb2640a8fbd65ddb9a67698940f522ac84b4b
Edwin Chen <edwinchen85@gmail.com>
2018-02-08 23:48:41 +0800

    Change localhost path to heroku path

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://enigmatic-lake-55144.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
